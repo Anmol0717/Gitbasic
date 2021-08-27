66825d8685b4888a1c6254ecf6b11f89f9a33513
Anmol Mehta <anmolmehta0717@gmail.com>
2021-08-27 16:49:30 +0530

    adding input_output.cpp

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    
+cout<<"enter 1st number : ";
+    cin >> num1; /* ">>" is called extraction operator*/
+cout<<"enter 2nd number : ";
+    cin >> num2; /* ">>" is called extraction operator*/
+
+    cout << "the sum is : " <<num1+num2;
+
+    return 0;
+}
